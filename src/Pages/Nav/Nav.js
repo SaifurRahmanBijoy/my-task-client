f14ffa65515afc852d5a6700b3e4620f71066ed2
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-12-28 15:21:50 +0600

    done dark mode

--- src/Pages/Nav/Nav.js
+++ src/Pages/Nav/Nav.js
@@ -2,19 +2,29 @@ import { Navbar } from "flowbite-react";
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import { CiDark } from "react-icons/ci";
 
 const Nav = () => {
-  const [theme, setTheme] = useState("light");
   const navigate = useNavigate();
+
+  const [theme, setTheme] = useState(localStorage.getItem("color-theme"));
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
-    } else {
+      localStorage.setItem("color-theme", "dark");
+    } else if (theme === "light") {
       document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
     }
   }, [theme]);
   const handleTheme = () => {
-    setTheme(theme === "dark" ? "light" : "dark");
+    if (theme === "dark") {
+      localStorage.setItem("color-theme", "light");
+      setTheme("light");
+    } else {
+      localStorage.setItem("color-theme", "dark");
+      setTheme("dark");
+    }
   };
 
   const { user, logOut } = useContext(AuthContext);
@@ -67,7 +77,7 @@ const Nav = () => {
   return (
     <div className="pb-2">
       <Navbar
-        className="bg-slate-300 dark:bg-slate-700 rounded-b-md shadow w-11/12 mx-auto px-10 md:px-10"
+        className="bg-slate-300 dark:bg-slate-700 rounded-b-md shadow md:w-11/12 mx-2 md:mx-auto md:px-10"
         fluid={true}
       >
         <Navbar.Brand>
@@ -80,18 +90,9 @@ const Nav = () => {
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="py-5 md:py-0">{menu}</Navbar.Collapse>
-        <label
-          htmlFor="default-toggle"
-          className="inline-flex relative items-center justify-center cursor-pointer mx-2"
-        >
-          <input
-            type="checkbox"
-            onClick={handleTheme}
-            id="default-toggle"
-            className="sr-only peer shadow-inner"
-          />
-          <div className="w-11 h-6 bg-gray-100 border border-slate-700 peer-focus:outline-none  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-red-400 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-400 peer-checked:bg-blue-800"></div>
-        </label>
+        <button className="bg-gray-300 border-b-2 hover:border border-black p-2 rounded-full dark:bg-blue-500 dark:border-white dark:text-white">
+          <CiDark onClick={handleTheme}></CiDark>
+        </button>
       </Navbar>
     </div>
   );
