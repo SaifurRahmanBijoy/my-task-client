4d913a6d622d5e536a2d008c64f9e81498ef7138
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-12-29 04:03:53 +0600

    Newly implemented SASS for styling

--- src/Pages/HomeLanding/Modal.js
+++ src/Pages/HomeLanding/Modal.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthProvider/AuthProvider";
+import "./Modal.scss";
 
 const Modal = ({ setShowModal }) => {
   const { user } = useContext(AuthContext);
@@ -32,13 +33,13 @@ const Modal = ({ setShowModal }) => {
   };
   return (
     <>
-      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+      <div className="my-own-styling overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+        <div className="relative my-6 mx-auto w-11/12 md:w-7/12">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white bg-opacity-70 dark:bg-slate-900 outline-none focus:outline-none">
+          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white bg-opacity-90 dark:bg-slate-900 outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-              <h3 className="text-2xl font-semibold dark:text-slate-400">
+              <h3 className="text-2xl font-semibold dark:text-slate-400 text-center">
                 Add task immediately
               </h3>
             </div>
@@ -83,7 +84,7 @@ const Modal = ({ setShowModal }) => {
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div className="fixed inset-0 z-40 bg-own"></div>
     </>
   );
 };
