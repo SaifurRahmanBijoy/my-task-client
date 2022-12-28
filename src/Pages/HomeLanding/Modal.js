28771e1371716fe7da3c025b936302f93ff45f1e
SaifurRahmanBijoy <saifurrahmanbijoy1@gmail.com>
2022-12-29 04:05:51 +0600

    done everything

--- src/Pages/HomeLanding/Modal.js
+++ src/Pages/HomeLanding/Modal.js
@@ -38,7 +38,7 @@ const Modal = ({ setShowModal }) => {
           {/*content*/}
           <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white bg-opacity-90 dark:bg-slate-900 outline-none focus:outline-none">
             {/*header*/}
-            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+            <div className="p-5 border-b border-solid border-slate-200 rounded-t">
               <h3 className="text-2xl font-semibold dark:text-slate-400 text-center">
                 Add task immediately
               </h3>
